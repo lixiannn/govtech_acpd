e62b553b60f421ba612d23ede252e73b165550e3
Li Xian <59865111+lixiannn@users.noreply.github.com>
2022-09-10 00:57:45 +0800

    added features: create account, login and logout

--- project/auth.py
+++ project/auth.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
-from flask_login import login_required
-
+from flask_login import login_user, login_required, current_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
 from . import db
 
 auth = Blueprint('auth', __name__)
@@ -8,13 +9,48 @@ auth = Blueprint('auth', __name__)
 @auth.route('/createAccount', methods=['POST'])
 @login_required
 def createAccount():
-    return 'Account created successfully'
+    # check if current_user is a prof
+    if current_user.role != 'professor':
+        return 'Only professors can create account'
+
+    # get info
+    account = request.get_json()
+    email = account['email']
+    full_name = account['full_name']
+    password = 'password'
+    role = 'student'
+
+    # check if user exists
+    user = User.query.filter_by(email=email).first()
+    if user:
+        return 'User already exists!'
+
+    # insert into db if user does not exist
+    new_user = User(email=email, full_name=full_name, password=generate_password_hash(password, method='sha256'), role=role)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return f'Account created successfully for {full_name}'
 
 @auth.route('/login', methods=['POST'])
 def login():
-    return 'Login Successfully'
+    # get info
+    account = request.get_json()
+    email = account['email']
+    password = account['password']
+    remember = True # remember user is logged in
+
+    # check if user exists and check pwd
+    user = User.query.filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        return 'Please check your login details & try again!'
+
+    # log user in
+    login_user(user, remember=remember)
+    return f'Login Successfully as {current_user.full_name}'
 
 @auth.route('/logout')
 @login_required
 def logout():
+    logout_user()
     return 'Logout Successfully'
--- project/models.py
+++ project/models.py
@@ -1,11 +1,15 @@
 from . import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     role = db.Column(db.String(100))
+
+    def get_id(self):
+           return (self.user_id)
 
 class Course_Result(db.Model):
     course_result_id = db.Column(db.Integer, primary_key=True) 
